d61af2436d4693788b55638d9be4a4fa7521f0ee
Greg Weil <weilg@rpi.edu>
2016-03-03 16:25:21 -0500

    Don't need to navigate to /feed.html directly

--- Lab 5/Solution/server.js
+++ Lab 5/Solution/server.js
@@ -5,6 +5,11 @@ var app = express();
 // Serve static files
 app.use(express.static('public'));
 
+// Serve the main page
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/feed.html');
+});
+
 // Listen for requests
 app.listen(3000, function() {
 	console.log("Server listening on port 3000");
