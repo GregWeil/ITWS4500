1dfdb870dc25a90fbf6add66c648bc96ce5b9c47
Greg Weil <weilg@rpi.edu>
2016-03-03 16:25:09 -0500

    Serve the previous assignment from the public directory

---
+++ Lab 5/Solution/public/js/feed.js
@@ -0,0 +1,27 @@
+///feed.js
+//Load a set of twitter posts and cycle through them
+
+var tweetFeedApp = angular.module('tweetFeedApp', ['ngAnimate']);
+
+tweetFeedApp.filter('sliceWrap', function() {
+	return function(input, count, start) {
+		return input.concat(input).slice(start, start+Math.min(count, input.length));
+	}
+});
+
+tweetFeedApp.controller('TweetFeedCtrl', ['$scope', '$http', '$interval',
+function($scope, $http, $interval) {
+	$scope.tagline = "recent but not live";
+	$scope.tweets = [];
+	$scope.index = 0;
+	
+	$http.get("get_tweets.php").then(function(response) {
+		$scope.tweets = response.data.statuses;
+	});
+	
+	$interval(function() {
+		if ($scope.tweets.length > 0) {
+			$scope.index = ($scope.index - 1 + $scope.tweets.length) % $scope.tweets.length;
+		}
+	}, 1000);
+}]);
--- Lab 5/Solution/server.js
+++ Lab 5/Solution/server.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var app = express();
 
+// Serve static files
+app.use(express.static('public'));
+
 // Listen for requests
 app.listen(3000, function() {
 	console.log("Server listening on port 3000");
