753b3c1f389c4cf908165ec480d3c8cbc06b82bf
Greg Weil <weilg@rpi.edu>
2016-03-02 23:22:16 -0500

    Scroll through two tweets at a time

--- Lab 4/Solution/js/feed.js
+++ Lab 4/Solution/js/feed.js
@@ -2,7 +2,15 @@
 //Load a set of twitter posts and cycle through them
 
 var tweetFeedApp = angular.module('tweetFeedApp', []);
-tweetFeedApp.controller('TweetFeedCtrl', function($scope, $http) {
+
+tweetFeedApp.filter('sliceWrap', function() {
+	return function(input, count, start) {
+		return input.concat(input).slice(start, start+Math.min(count, input.length));
+	}
+});
+
+tweetFeedApp.controller('TweetFeedCtrl', ['$scope', '$http', '$interval',
+function($scope, $http, $interval) {
 	$scope.tagline = "recent but not live";
 	$scope.tweets = [];
 	$scope.index = 0;
@@ -10,4 +18,10 @@ tweetFeedApp.controller('TweetFeedCtrl', function($scope, $http) {
 	$http.get("get_tweets.php").then(function(response) {
 		$scope.tweets = response.data.statuses;
 	});
-});
+	
+	$interval(function() {
+		if ($scope.tweets.length > 0) {
+			$scope.index = ($scope.index - 1 + $scope.tweets.length) % $scope.tweets.length;
+		}
+	}, 1000);
+}]);
