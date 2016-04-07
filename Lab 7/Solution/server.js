f882aa0f3678c9386dbd62dd2b063dd39c4d0826
Greg Weil <weilg@rpi.edu>
2016-04-07 16:24:14 -0400

    Moved twitter api streaming code into a separate file

--- Lab 7/Solution/server.js
+++ Lab 7/Solution/server.js
@@ -1,9 +1,9 @@
 // Required modules
 var express = require('express');
 var bodyParser = require('body-parser');
-var Twitter = require('twitter');
 var fs = require('fs');
 
+var twitter = require('./twitterStream.js');
 
 // Init the server
 var app = express();
@@ -18,59 +18,11 @@ app.get('/', function(req, res) {
 });
 
 
-// Create the API link
-var config = {
-	access_token_key: '47282406-UyxyVM0rkAwuti3YpgwLGyi84Yh8KXxKOv4mQ6Xhv',
-    access_token_secret: 'DLEtDS9DFGpPUVG1SVC5A4J15iTd8VPZ68HmOU3BJmtGx',
-    consumer_key: 'luI2jnjp3GOuyqKuWsSKIFstD',
-    consumer_secret: 'HvEmsAiI8M5TE972F081eBgZDLR8pJnTg2UFdqXs7SC3WpzehT'
-};
-var client = new Twitter(config);
-
-function queryBuild(req) {
-	var query = {};
-	if (req.track) query['track'] = req.track;
-	if (req.follow) query['follow'] = req.follow;
-	if (!('track' in query) && !('follow' in query)) {
-		query.locations = "42.72,-73.68,42.73,-73.67";
-	}
-	return query;
-}
-
-function queryAPI(query, count, output, start, write, end) {
-	console.log("requested " + count);
-	client.stream("statuses/filter", query, function(stream) {
-		start(output);
-		var first = true;
-		
-		//Receive a tweet
-		stream.on('data', function(tweet) {
-			write(output, tweet, first);
-			first = false;
-			if (--count <= 0 ) {
-				end(output, true);
-				stream.destroy( );
-				console.log("done");
-			} else {
-				console.log(count + " remaining");
-			}
-		});
-		
-		//Error, Close the stream and close out the response
-		stream.on('error', function(error) {
-			end(output, false);
-			stream.destroy();
-			console.log(error);
-		});
-	});
-}
-
-
 // API querying
 app.get('/query', function(req, res) {
-	var query = queryBuild(req.query);
+	var query = twitter.queryBuild(req.query);
 	var count = req.query.count || 1;
-	queryAPI(query, count, res,
+	twitter.queryAPI(query, count, res,
 		function(stream) {
 			stream.write("[");
 		},
@@ -87,7 +39,7 @@ app.get('/query', function(req, res) {
 
 // API export
 app.post('/export', function(req, res) {
-	var query = queryBuild(req.body);
+	var query = twitter.queryBuild(req.body);
 	var count = req.body.count || 1;
 	
 	var fdate = new Date();
@@ -105,7 +57,7 @@ app.post('/export', function(req, res) {
 	var file = fs.createWriteStream("./public/" + fname);
 	
 	if (req.body.format == 'csv') {
-		queryAPI(query, count, file,
+		twitter.queryAPI(query, count, file,
 			function(stream) {
 				stream.write('"id","text","created_at",');
 				stream.write('"user_id","user_name","user_screen_name",');
@@ -134,7 +86,7 @@ app.post('/export', function(req, res) {
 			}
 		);
 	} else {
-		queryAPI(query, count, file,
+		twitter.queryAPI(query, count, file,
 			function(stream) {
 				stream.write("[");
 			},
