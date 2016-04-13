d0f99058e3e2647a946238a231f229f908ea5933
Greg Weil <weilg@rpi.edu>
2016-04-13 16:42:51 -0400

    Connect to the mongodb server

--- Lab 7/Solution/server.js
+++ Lab 7/Solution/server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 
+var MongoClient = require('mongodb').MongoClient;
+
 var twitter = require('./twitterStream.js');
 var twitterExport = require('./twitterExport.js');
 
@@ -58,6 +60,13 @@ app.post('/export', function(req, res) {
 	} else {
 		twitter.queryAPI(query, count, file, twitterExport.json, done);
 	}
+});
+
+
+// Database usage
+MongoClient.connect('mongodb://localhost:27017', function(err, db) {
+	if (err) console.log(err);
+	else console.log('connected');
 });
 
 
