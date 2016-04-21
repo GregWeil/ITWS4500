14d98a513069b0832918877f3e2594475b52460f
Greg Weil <weilg@rpi.edu>
2016-04-20 21:06:12 -0400

    Send a canned query to the SPARQL endpoint

--- Lab 8/Solution/server.js
+++ Lab 8/Solution/server.js
@@ -2,6 +2,7 @@
 
 //Required modules
 var express = require('express')
+var request = require('request')
 
 //Initialize the server
 var port = 3000
@@ -12,7 +13,13 @@ app.use(express.static('public'))
 
 //Query endpoint
 app.get('/query', function(req, res) {
-    res.send(req.query)
+    request.get({
+        url: 'http://dbpedia.org/sparql',
+        qs: {
+            query: 'select+distinct+%3FConcept+where+%7B%5B%5D+a+%3FConcept%7D+LIMIT+100',
+            format: 'application/sparql-results+json'
+        }
+    }).pipe(res)
 })
 
 //Open the server to requests
