0f1c337465e4ee9f2b91fc1da75e06f83b93e14d
Greg Weil <weilg@rpi.edu>
2016-05-03 18:35:23 -0400

    Use angular in the analysis page

--- Lab 10/Solution/public/js/analyze.js
+++ Lab 10/Solution/public/js/analyze.js
@@ -1,15 +1,22 @@
 ///analyze.js
 //Load a set of twitter posts and cycle through them
 
-function reload() {
-	$.get("db/read").done(function(data) {
-		var tweetCount = $('#tweetCount')
-		tweetCount.text('Loaded ' + data.length + ' tweet');
-		if (data.length > 1) tweetCount.text(tweetCount.text() + 's');
-		if (data.length <= 0) tweetCount.text('No tweets found yet');
-		console.log(data[0]);
-		setTimeout(reload, 1000);
-	});
-}
+var tweetAnalyzeApp = angular.module('tweetAnalyzeApp', []);
 
-$(document).ready(reload);
+tweetAnalyzeApp.controller('tweetAnalyzeCtrl',
+	['$scope', '$http', '$timeout',
+	function($scope, $http, $timeout) {
+		$scope.tweets = [];
+		
+		$scope.reload = function() {
+			$http.get("db/read").then(function(response) {
+				$scope.tweets = response.data;
+				console.log($scope.tweets[0]);
+				
+				$timeout($scope.reload, 1000);
+			});
+		}
+		
+		$scope.reload();
+	}]
+);
