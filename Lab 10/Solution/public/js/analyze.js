c781e1aa3112d5145457655d86433312558f8e5e
Greg Weil <weilg@rpi.edu>
2016-05-04 01:38:26 -0400

    Pull hashtag data

--- Lab 10/Solution/public/js/analyze.js
+++ Lab 10/Solution/public/js/analyze.js
@@ -23,10 +23,31 @@ tweetAnalyzeApp.controller('tweetAnalyzeCtrl',
 			}
 		};
 		
+		$scope.scanHashtags = function() {
+			var tags = {}
+			for (var i = 0; i < $scope.tweets.length; ++i) {
+				var tweet = $scope.tweets[i];
+				for (var j = 0; j < tweet.entities.hashtags.length; ++j) {
+					var hashtag = tweet.entities.hashtags[j].text;
+					if (!tags[hashtag]) tags[hashtag] = 0;
+					tags[hashtag] += 1;
+				}
+			}
+			
+			$scope.hashtags.labels = Object.keys(tags).sort(function(a, b) {
+				return (tags[b] - tags[a]);
+			});
+			$scope.hashtags.data = $scope.hashtags.labels.map(function(tag) {
+				return tags[tag];
+			});
+		};
+		
 		$scope.reload = function() {
 			$http.get("db/read").then(function(response) {
 				$scope.tweets = response.data;
 				console.log($scope.tweets[0]);
+				
+				$scope.scanHashtags();
 				
 				$timeout($scope.reload, 1000);
 			});
